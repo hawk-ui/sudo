eab198fcd354d03f3b1751e84ce1566a65f82a61
Thomas Boerger <tboerger@tbpro.de>
2013-10-18 16:06:30 +0200

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: sudo
+# Attributes:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["sudo"]["packages"] = %w(
+  sudo
+)
+
+default["sudo"]["keep_env"] = %w(
+  LANG 
+  LC_ADDRESS 
+  LC_CTYPE 
+  LC_COLLATE
+  LC_IDENTIFICATION 
+  LC_MEASUREMENT 
+  LC_MESSAGES 
+  LC_MONETARY 
+  LC_NAME 
+  LC_NUMERIC 
+  LC_PAPER 
+  LC_TELEPHONE 
+  LC_TIME 
+  LC_ALL 
+  LANGUAGE 
+  LINGUAS 
+  XDG_SESSION_COOKIE
+)
+
+default["sudo"]["secure_path"] = %w(
+  /usr/local/sbin
+  /usr/local/bin
+  /usr/sbin
+  /usr/bin
+  /sbin
+  /bin
+)
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "sudo"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures sudo"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "sudo", "Installs/Configures sudo"
+
+supports "suse"
