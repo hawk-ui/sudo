4879184c49670fdd980584ba50137e09f0900644
Thomas Boerger <thomas@webhippie.de>
2014-10-19 22:38:11 +0200

    Updated copyright header

--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: sudo
 # Resource:: default
 #
-# Copyright 2013, Thomas Boerger
+# Copyright 2013-2014, Thomas Boerger <thomas@webhippie.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
